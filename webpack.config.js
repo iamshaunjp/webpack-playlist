8c751bb13cd98c507bac8823edb5f1db8249e81f
Shaun Pelling <shaunjp@shaun-pelling.com>
2016-09-26 13:07:06 +0100

    lesson code added

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    //define entry point
+    entry: './src/script-1.js',
+
+    //defin output point
+    output: {
+        path: 'dist',
+        filename: 'bundle.js'
+    }
+
+};
